9ef90d46a8e7e332709fc6ccbe2c7cd1118486ac
jihwangim09-design <jihwangim09@gmail.com>
2026-08-03 12:19:36 +0900

    >

--- src/day04/Exam2.java
+++ src/day04/Exam2.java
@@ -18,14 +18,26 @@ public class Exam2 {
 
         // 3. 무한루프 : 실행문 계속반복, 특정조건에 따른 break 활용
         for ( ; ; ){System.out.println("무한반복"); break; }
+        while (true) { System.out.println("무한반복"); break; }
 
         // 4. 초기값; while( 조건문 ){ 실행문; } [VS] for문
-        int i = 1; // [1] 초기값
-        while ( i <= 10 ) { // [2] 조선식
+        int j = 1; // [1] 초기식
+        while ( j <= 10 ) { // [2] 조선식
 
             // [3] 증감식
-            i++;
+            j++;
         } // w end
+
+        // 5. 번복문과 배열 관계 : 배열내 저장된 자료들을 인덱스(저장된순서번호)로 식별 ( 0 ~ )
+        int[ ] ary = { 92 , 80 , 75 }; // 자바 배열 [VS] 자바스크립트배열
+        // 배열은 인덱스가 0부터 마지막 인덱스까지 순차 저장
+        for ( int 인덱스 = 0 ; 인덱스 <= ary.length - 1 ; 인덱스++) { 
+            System.out.println( ary[인덱스]);
+        }
+        // 향상된 for문 , for( 타입명 변수명 : 배열명 ){ } , 배열내 : 콜론 기준 오른쪽 배열내 요소들을 왼쪽 변수에 하나씩 대입
+        for ( int data : ary ){ // : 기준으로 오른쪽인 ary의 배열내 요소들을 하나씩 왼쪽 변수인 data에 대입을 한다
+            System.out.println(data);
+        }
 
     } //
 } // 

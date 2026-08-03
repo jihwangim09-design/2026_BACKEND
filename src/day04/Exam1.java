f4651aa30ec2444f445d05df7723d864693943a9
jihwangim09-design <jihwangim09@gmail.com>
2026-08-03 10:03:28 +0900

    >

--- src/day04/Exam1.java
+++ src/day04/Exam1.java
@@ -3,13 +3,13 @@ package day04;
 public class Exam1 {
     public static void main(String[] args) {
         
-        // 1. 조건문1. : if( true/false ) 실행문;
+        // 조건문1. : if( true/false ) 실행문;
         int 온도 = 5;
         if( 온도 <= 10 ) System.out.print("외투 입는다.");
 
-        // 2. 조건문1. : if( true/false ) 실행문;
-        int 온도 = 5;
-        if( 온도 <= 10 ) System.out.print("외투 입는다.");
+        // 조건문2. : if( true/false ) ( 실행문; , 실행문; )
+        if ( 온도 <= 10 ){ System.out.println("외투 입는다.");}
+        if( 온도 <= 10 )  System.out.print("외투 입는다.");
 
 
     }

2a5540d628e1bf27be9380da60082bec37f300fc
jihwangim09-design <jihwangim09@gmail.com>
2026-08-05 15:22:05 +0900

    >

---
+++ src/DATEBASE/day02/day02.sql
@@ -0,0 +1,7 @@
+DROP DATABASE IF EXISTS mvdb0805; # [1] 데이터베이스 삭제 # ctrl+엔터 (맥북은 커맨더+엔터)
+
+CREATE DATABASE mydb0805; #[2] 데이터베이스 생성
+
+SHOW DATABASES; #[3] 데이터베이스 목록
+
+USE mydb0805; # [4] 사용할 데이터베이스 목록

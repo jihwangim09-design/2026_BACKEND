365c041d72ea1d5c2c679603a8e44f68f6e4149c
jihwangim09-design <jihwangim09@gmail.com>
2026-08-06 16:34:49 +0900

    >

--- src/day06/OverallController.java
+++ src/day06/OverallController.java
@@ -1,63 +1,63 @@
-package day06;
-
-import java.net.Socket;
 import java.util.Scanner;
-
 public class OverallController {
     public static void main(String[] args) {
+        OverallRepository repository = new OverallRepository();
         Scanner scan = new Scanner(System.in);
-        Post[] posts = new Post[100];
-
-        for(;;){
-            System.out.println("===My Community===");
-            System.out.println("1.게시물 쓰기 2.게시물 출력");
-            System.out.println("==================");
-            System.out.printf("선택");
+        for (;;) {
+            System.out.println("============ My Community ============ ");
+            System.out.println("1.게시물쓰기 2.게시물출력");
+            System.out.println("====================================== ");
+            System.out.print("선택>");
             int ch = scan.nextInt();
 
-            if(ch==1){
+            if (ch == 1) {
                 scan.nextLine();
-                System.out.println("작성자");
-                String writer = scan.nextLine();
-                System.out.println("내용");
-                String content = scan.nextLine();
+                System.out.print("내용 : ");     String content = scan.nextLine();
+                System.out.print("작성자 : ");   String writer = scan.nextLine();
 
-                Post post = new Post(writer, content);
-                boolean result = false;
+                Post post = new Post(content, writer);
+                boolean result = repository.save(post);
 
-                for ( int i = 0; i<=posts.length-1 ; i++){
-                    if (posts[i]==null){
-                        posts[i]=post;
-                        result = true;
-                        break;
+                if (result) { System.out.println("[안내] 글쓰기 성공");} 
+                else { System.out.println("[안내] 글쓰기 실패"); }
+
+            } else if (ch == 2) {
+                Post[] posts = repository.findAll();
+                for (Post post : posts) {
+                    if (post != null) {
+                        System.out.printf("작성자 : %s , 내용 : %s \n", post.writer, post.content);
                     }
-                }
-                
-                if(result){
-                    System.out.println("[안내] 게시글 쓰기 성공!");
-                }else{System.out.println("[안내] 게시글 쓰기 실패");}
-            }else if(ch==2){
-                for(Post post : posts){
-                    if(post!=null){
-                        System.out.printf("작성자: 내용: \n" , post.writer , post.content );
-                    }
-                    
                 }
             }
         }
-
-    }// med
-} // c ed
-
-class Post{
-    String writer;
-    String content;
-
-    Post(){}
-
-    Post(String writer, String content){
-        this.writer = writer;
-        this.content = content;
     }
 }
+// 데이터 저장 및 반환(조회) 비즈니스 로직 전담 클래스
+class OverallRepository {
 
+    Post[] posts = new Post[100];
+
+    // 게시물 저장
+    boolean save(Post post) {
+        for (int index = 0; index < posts.length; index++) {
+            if (posts[index] == null) {
+                posts[index] = post;
+                return true;
+            }
+        }
+        return false;
+    }
+    // 게시물 전체 목록 반환
+    Post[] findAll() {
+        return posts;
+    }
+}
+class Post {
+    String content;
+    String writer;
+    Post() {}
+    Post(String content, String writer) {
+        this.content = content;
+        this.writer = writer;
+    }
+}

4cd98c570a2cdf7d07620c0765e1d2726527dda5
jihwangim09-design <jihwangim09@gmail.com>
2026-08-06 17:11:15 +0900

    >

--- test/Minipj1.java
+++ test/Minipj1.java
@@ -2,6 +2,66 @@ package test;
 
 import java.util.Scanner;
 
+public class Minipj1 {
+    public static void main(String[] args) {
+        StudyRepository studies = new StudyRepository();
+        UserRepository users = new UserRepository();
+        Scanner scan = new Scanner(System.in);
+        for (;;) {
+            System.out.println("============ My Community ============ ");
+            System.out.println("1.글쓰기 2.글출력 3.회원가입 4.회원목록");
+            System.out.println("====================================== ");
+            System.out.print("선택>");
+            int ch = scan.nextInt();
+
+            if(ch==1){
+                scan.nextLine();
+                System.out.print("제목 : ");   String title = scan.nextLine();
+                System.out.print("회원번호(1~4) : ");   int fk_userNo = scan.nextInt();
+                System.out.print("공부카테고리(1~10) : ");   int fk_categoryNo = scan.nextInt();
+                System.out.print("진행방식(1~4) : ");   int fk_studyTypeNo = scan.nextInt();
+                System.out.print("모집인원 : ");   int maxMember = scan.nextInt();
+                System.out.print("상세내용 : ");   String detail = scan.nextLine();
+                System.out.print("등록일을 입력해주세요(XXXX년XX월XX일) : ");   String date = scan.nextLine();
+
+                Study study1 = new Study(title, fk_userNo, fk_categoryNo, fk_studyTypeNo, maxMember, detail, date);
+                boolean result = studies.save(study1);
+
+                if(result){System.out.println("게시물 작성 성공");}
+                else{System.out.println("게시물 작성 실패");}
+                
+            }else if(ch==2){
+                Study[] result = studies.findAll();
+                for(Study study1 : result){
+                    if(study1 != null){
+                        System.out.printf("제목 : %s , 회원번호(1~4) : %d 공부카테고리(1~10) : %d , 진행방식(1~4) : %d 모집인원 : %d , 상세내용 : %s 등록일을 입력해주세요(XXXX년XX월XX일) : %s \n", study1.title, study1.fk_userNo, study1.fk_categoryNo, study1.fk_studyTypeNo, study1.maxMember, study1.detail, study1.date );
+                    }
+                }
+            }else if(ch==3){
+                scan.nextLine();
+                System.out.print("ID : ");   String userId = scan.nextLine();
+                System.out.print("PW : ");   String userPw = scan.nextLine();
+                System.out.print("이름 : ");   String userName = scan.nextLine();
+                User user = new User(userId, userPw, userName);
+                boolean result = users.save(user);
+
+                if(result){System.out.println("회원가입 성공");}
+                else{System.out.println("회원가입 실패");}
+
+            }else if (ch == 4) {
+                User[] result = users.findAll();
+                for (User user : result) {
+                    if (user != null) {
+                        System.out.printf("ID : %s , PW : %s , 이름 : %s \n", user.userId, user.userPw, user.userName);
+                    }
+                }   
+            }
+        }
+    }
+}
+
+
+
 class Study{
     int contentNo;
     String title;
@@ -93,61 +153,4 @@ class UserRepository{
     User[] findAll() {
         return users;
     }
-}
-public class Minipj1 {
-    public static void main(String[] args) {
-        StudyRepository studies = new StudyRepository();
-        UserRepository users = new UserRepository();
-        Scanner scan = new Scanner(System.in);
-        for (;;) {
-            System.out.println("============ My Community ============ ");
-            System.out.println("1.글쓰기 2.글출력 3.회원가입 4.회원목록");
-            System.out.println("====================================== ");
-            System.out.print("선택>");
-            int ch = scan.nextInt();
-
-            if(ch==1){
-                scan.nextLine();
-                System.out.print("제목 : ");   String title = scan.nextLine();
-                System.out.print("회원번호(1~4) : ");   int fk_userNo = scan.nextInt();
-                System.out.print("공부카테고리(1~10) : ");   int fk_categoryNo = scan.nextInt();
-                System.out.print("진행방식(1~4) : ");   int fk_studyTypeNo = scan.nextInt();
-                System.out.print("모집인원 : ");   int maxMember = scan.nextInt();
-                System.out.print("상세내용 : ");   String detail = scan.nextLine();
-                System.out.print("등록일을 입력해주세요(XXXX년XX월XX일) : ");   String date = scan.nextLine();
-
-                Study study1 = new Study(title, fk_userNo, fk_categoryNo, fk_studyTypeNo, maxMember, detail, date);
-                boolean result = studies.save(study1);
-
-                if(result){System.out.println("게시물 작성 성공");}
-                else{System.out.println("게시물 작성 실패");}
-                
-            }else if(ch==2){
-                Study[] result = studies.findAll();
-                for(Study study1 : result){
-                    if(study1 != null){
-                        System.out.printf("제목 : %s , 회원번호(1~4) : %d 공부카테고리(1~10) : %d , 진행방식(1~4) : %d 모집인원 : %d , 상세내용 : %s 등록일을 입력해주세요(XXXX년XX월XX일) : %s \n", study1.title, study1.fk_userNo, study1.fk_categoryNo, study1.fk_studyTypeNo, study1.maxMember, study1.detail, study1.date );
-                    }
-                }
-            }else if(ch==3){
-                scan.nextLine();
-                System.out.print("ID : ");   String userId = scan.nextLine();
-                System.out.print("PW : ");   String userPw = scan.nextLine();
-                System.out.print("이름 : ");   String userName = scan.nextLine();
-                User user = new User(userId, userPw, userName);
-                boolean result = users.save(user);
-
-                if(result){System.out.println("회원가입 성공");}
-                else{System.out.println("회원가입 실패");}
-
-            }else if (ch == 4) {
-                User[] result = users.findAll();
-                for (User user : result) {
-                    if (user != null) {
-                        System.out.printf("ID : %s , PW : %s , 이름 : %s \n", user.userId, user.userPw, user.userName);
-                    }
-                }
-        }
-    }
-}
 }

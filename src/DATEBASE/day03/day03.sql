8699e7e12e828a07b57d94e602104553b475b730
jihwangim09-design <jihwangim09@gmail.com>
2026-08-07 15:16:15 +0900

    >

--- src/DATEBASE/day03/day03.sql
+++ src/DATEBASE/day03/day03.sql
@@ -17,7 +17,23 @@ INSERT INTO test( 이름 , 개수 ) VALUES( "강호동" , 20 ); -- 번호 값 �
 INSERT INTO test( 이름 ) VALUES( "신동엽" ); -- 번호(auto) , 개수(defalut) 제외한 삽입
 -- INSERT INTO test( 이름 ) VALUES( "유재석" ) ; -- 이름은 중복 불가능 / 오류
 INSERT INTO test VALUES( 4, "하하" , 30 ); -- 모든 속성값을 정의 순서대로 값 대입시 속성명 생략
-INSERT INTO test( 이름 ) VALUES( "박명수" ) , ( "수박" ) , ( "바나나" );
+INSERT INTO test( 이름 ) VALUES( "박명수" ) , ( "수박" ) , ( "바나나" ); -- 3개의 레코드를 한번에 삽입
 
-# [2] select : select * from 테이블명
-SELECT * from test;
+# [2] select : select * from 테이블명 where 조건
+SELECT * from test; -- 테이블내 모든 속성의[*(와일드카드)] 레코드 조회
+SELECT 이름 from test; -- 테이블내 '이름' 속성명의 레코드
+SELECT 이름 , 개수 from test; -- '이름' '개수' 속성명의 레코드 조회
+SELECT * FROM test WHERE 이름 = "유재석"; -- 프로그래밍이 아니라 대입이 없음 그래서 =은 대입이 아니라 같다 라는 의미
+SELECT * FROM test WHERE 개수 >= 5; -- 개수 속성명의 값이 5이상이면 조회
+
+# [3] update : update 테이블명 set 속성명 = 새로운값 , 속성명 = 새로운값 where 조건
+UPDATE test SET 개수 = 10; -- 조건이 없으므로 테이블내 '개수'속성들의 값이 모두 10으로 수정
+UPDATE test SET 개수 = WHERE 이름 = "유재석"; -- 이름 속성값이 유재석 이면 개수 속성값을 10으로 수정
+UPDATE test SET 개수 = 40 , 이름 = "강호동2" WHERE 번호 = 2; -- 번호속성값이 2이면 개수속성값을 40, 이름속성값을 강호동2 수정
+
+# [4] delete : delete from 테이블명 where 조건
+-= delete from test; -- 조건이 없으므로 테이블내 모든 레코드 제거
+DELETE from test where 이름 = "유재석"; -- 이름 속성값이 "유재석"이면 삭제
+delete from test WHERE 번호 =  2; -- 번호 속성값이 2 이면 삭제
+-- (DML) delete : 테이블내 레코드 삭제 [vs] (DML) truncate table : 테이블내 레코드 삭제 [vs] drop table : 테이블 자체 삭제
+-- DDL은 취소(ROLLBACK)불가능 , DML은 취소(ROLLBACK)가능

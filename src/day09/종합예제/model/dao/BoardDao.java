9feea2e383a4148b8cdf94a4871c8fb9e1610539
jihwangim09-design <jihwangim09@gmail.com>
2026-08-10 13:21:58 +0900

    0810 13:21

--- src/day09/종합예제/model/dao/BoardDao.java
+++ src/day09/종합예제/model/dao/BoardDao.java
@@ -1,8 +1,14 @@
 package day09.종합예제.model.dao;
 
+import java.util.ArrayList;
+
+import day09.종합예제.model.dto.BoardDto;
+
 public class BoardDao {
     private BoardDao(){};
     private static final BoardDao instance = new BoardDao();
     public static BoardDao getInstance (){ return instance;}
-    // *추후에 MYSQL 서버와연동*
+    // * 추후에 MYSQL 서버와연동*
+    // * 데이터베이스 대신에 ArrayList 사용 *
+    private ArrayList< BoardDto > boardList = new ArrayList<>();
 }

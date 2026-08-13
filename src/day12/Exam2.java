f2eb361950f8d280a4975ec6faa684bbf0b8f1de
jihwangim09-design <jihwangim09@gmail.com>
2026-08-13 13:17:58 +0900

    0813 1317

--- src/day12/Exam2.java
+++ src/day12/Exam2.java
@@ -3,6 +3,7 @@ package day12;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class Exam2 {
@@ -21,7 +22,7 @@ public class Exam2 {
             Connection conn = DriverManager.getConnection( url , user , password ); // DriverManager가 구현체 
             // [3] 데이터베이스 내 DML(insert,select,update,delete) 조작
             // 1. insert 하기
-            String sql = "insert into test( name ) values( '유재석') "; // 자바 문자열에 SQL 작성(자동완성 안된다.)
+            String sql = "insert into test( name ) values('유재석') "; // 자바 문자열에 SQL 작성(자동완성 안된다.)
             PreparedStatement ps = conn.prepareStatement(sql); 
             // conn(연동된인터페이스).prepareStatement( 기재할SQL ); , 기재된 인터페이스 표현
             // PreparedStatement( SQL이 기재된 인터페이스 )
@@ -31,9 +32,13 @@ public class Exam2 {
             // 2. select 하기 , select 필드명 from 테이블명;
             String sql2 = "select * from test";
             PreparedStatement ps2 = conn.prepareStatement(sql2); // 작성한 SQL 기재한다. 
-            ps2.executeQuery(); // 기재된 SQL 실행
+            ResultSet rs = ps2.executeQuery(); // 기재된 SQL 실행 , 결과물 rs(ResultSet) 인터페이스 대입
+            rs.next(); // 커서를 다음 레코드로 이동 시킴 /조회 결과에서 다음레코드 이동
+            System.out.println(rs.getInt("no")); //rs.get타입("속성명");
+            System.out.println(rs.getString("name")); //rs.get타입("속성명");
+            System.out.println("레코드 조회 성공");
 
-            
+
         }catch( ClassNotFoundException e ){
             System.out.println("드라이버가 존재하지 않습니다."+ e); // 라이브러리 설치 안함
         }catch( SQLException e ){

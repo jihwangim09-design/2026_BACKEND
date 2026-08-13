e5a0cee0e06831f9cac4c68970b5677900a90caf
jihwangim09-design <jihwangim09@gmail.com>
2026-08-13 12:49:29 +0900

    0813 1249

---
+++ bin/day12/sample.sql
@@ -0,0 +1,5 @@
+-- day12 Exam2와 연동할 DB 생성
+CREATE DATABASE mydb0813;
+use mydb0813;
+CREATE Table test( no int AUTO_INCREMENT PRIMARY KEY , name VARCHAR(30) );
+
--- src/day12/Exam2.java
+++ src/day12/Exam2.java
@@ -1,6 +1,8 @@
 package day12;
 
+import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Exam2 {
     public static void main(String[] args) {
@@ -11,10 +13,17 @@ public class Exam2 {
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
             System.out.println("Driver 연동성공");
-            //[2] 데이터베이스 서버 연동
+            //[2] 데이터베이스 서버 연동 * 예외처리 필수 *
             // DriverManager.getConnection( "jdbc:mysql://ip번호:3306/데이터베이스명" , "계정명" , "비밀번호" )
+            String url = "jdbc:mysql://127.0.0.1:3306/mydb0813";
+            String user = "root"; String password = "1234";
+            Connection conn = DriverManager.getConnection( url , user , password ); // DriverManager가 구현체 
+            // [3] 데이터베이스 내 DML(insert,select,update,delete) 조작
+            System.out.println("데이터베이스 연동성공");
         }catch( ClassNotFoundException e ){
-            System.out.println("드라이버가 존재하지 않습니다."+e); // 라이브러리 설치 안함
+            System.out.println("드라이버가 존재하지 않습니다."+ e); // 라이브러리 설치 안함
+        }catch( SQLException e ){
+            System.out.println("DB서버와 연동 실패했다." + e);
         }
     }
     

fa00cfce74565f22bdeb512364c328d4bc8871ae
jihwangim09-design <jihwangim09@gmail.com>
2026-08-19 09:45:12 +0900

    0819 0945

---
+++ src/day13/Exam1.java
@@ -0,0 +1,14 @@
+package day13;
+
+public class Exam1 {
+    public static void main(String[] args) {
+        
+    } // m ed
+} // c ed
+
+/* 
+    기본타입 : byte short int long float double shar boolean
+        - 리터럴 = 상수
+    참조타입 : 클래스(String,Scanner,~DTO 등등), 인터페이스(Connetion 등등), 배열[ ]
+        - 참조 = 주소
+    */

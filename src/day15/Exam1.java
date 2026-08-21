e7c3e1525a72bc668c7fd14a2d18e663815d6b50
jihwangim09-design <jihwangim09@gmail.com>
2026-08-21 09:49:04 +0900

    0821 0949

---
+++ src/day15/Exam1.java
@@ -0,0 +1,17 @@
+package day15;
+
+public class Exam1 {
+    public static void main(String[] args) {
+        /*
+
+            제네릭타입: 클래스 만들때 타입 정하지 않고 클래스 사용할때 타입 정한다.
+                List<BoradDto> list = new ArrayList<>()
+                - List 인터페이스를 만들떄 정의하지 않고 List 인터페이스를 사용할 때 BoardDto라고 타입을 정했다.
+
+            컬렉션프레임워크
+                List 인터페이스:    중복허용, 인덱스(순서)있음, ArrayList , LinkedList , Vector 등
+                Set 인터페이스:     중복불가, 인덱스(순서)없음
+                Map 인터페이스:     
+        */  
+    } // main ed
+} // class ed

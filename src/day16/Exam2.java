fe9d38c289a7a57838b746aeaac4f28bbfe1afe7
jihwangim09-design <jihwangim09@gmail.com>
2026-08-24 11:53:59 +0900

    0824 1153

--- src/day16/Exam2.java
+++ src/day16/Exam2.java
@@ -12,11 +12,7 @@ public class Exam2 {
             구조: 선입선출(queue)방식으로 먼저 요청이 들어온 순서대로 *대기중인* 스레드가 처리한다.
             만일 대기중인 스레드가 없으면 요청은 wait(대기) 상태이며 스레드가 작업이 끝나고 대기중인 요청 처리
             활용처: 웹서버(톰캣), JDBC , 콜센터 시스템 , 안전한 서버 구축 등등
-
-
-
         */
-
         // [1] 스레드풀 구축( 미리 스레드 수 생성) ,
         // ThreadPoolExecuto poolExecutor = (ThreadPoolExecutor)Executors.newFixedThreadPool(3);
         ThreadPoolExecutor poolExecutor = (ThreadPoolExecutor)Executors.newFixedThreadPool(3); // 강제 타입 변환 3개의 스레드 풀 구축
